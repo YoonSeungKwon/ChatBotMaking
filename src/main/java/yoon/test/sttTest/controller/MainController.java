cf7ae27a140fbc70728b03bee3489c829b6b19b8
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-09-27 17:52:44 +0900

    /STT

--- src/main/java/yoon/test/sttTest/controller/MainController.java
+++ src/main/java/yoon/test/sttTest/controller/MainController.java
@@ -1,32 +1,79 @@
 package yoon.test.sttTest.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/stt")
 public class MainController {
 
-    @Value("${user.client-id}")
+    @Value("${naver.client-id}")
     private String CLIENT_ID;
-    @Value("${user.secret}")
+    @Value("${naver.secret}")
     private String SECRET;
+    @GetMapping("/")
+    public ResponseEntity<?> stt() {
 
-    @PostMapping("/")
-    public ResponseEntity<?> stt(HttpServletRequest request, HttpServletResponse response){
-        response.setHeader("X-NCP-APIGW-API-KEY-ID", CLIENT_ID);
-        response.setHeader("X-NCP-APIGW-API-KEY", SECRET);
-        response.setHeader("Content-Type", "application/octet-stream");
+        String result = "";
 
+        try {
+            String imgFile = "src/main/resources/static/test.mp3";
+            File voiceFile = new File(imgFile);
 
+            String language = "Kor";        // 언어 코드 ( Kor, Jpn, Eng, Chn )
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + language;
+            URL url = new URL(apiURL);
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
+            conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", CLIENT_ID);
+            conn.setRequestProperty("X-NCP-APIGW-API-KEY", SECRET);
+
+            OutputStream outputStream = conn.getOutputStream();
+            FileInputStream inputStream = new FileInputStream(voiceFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            BufferedReader br = null;
+            int responseCode = conn.getResponseCode();
+            if(responseCode == 200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {  // 오류 발생
+                System.out.println("error!!!!!!! responseCode= " + responseCode);
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            }
+            String inputLine;
+
+            if(br != null) {
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                result = response.toString();
+            } else {
+                result = "error";
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return ResponseEntity.ok(result);
     }
 
 }
